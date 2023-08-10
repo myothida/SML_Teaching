6da60f3640b57e5f781c0e968c202aeba039aea9
myo <drmyothida@gmail.com>
2023-08-10 11:52:16 +0700

    add main and predict for deployment

---
+++ CMU_Sem01_2023/condoPrice_predict.py
@@ -0,0 +1,17 @@
+import joblib
+import pandas as pd
+from sklearn.pipeline import Pipeline
+
+model = joblib.load("CNX_BestModel.pkl")
+
+def make_prediction(inputs):
+    """
+    Make a prediction using the trained model
+    """
+    inputs_df = pd.DataFrame(
+        inputs,
+        columns=["sepal_length_cm", "sepal_width_cm", "petal_length_cm", "petal_width_cm"]
+        )
+    predictions = model.predict(inputs_df)
+  
+    return predictions
